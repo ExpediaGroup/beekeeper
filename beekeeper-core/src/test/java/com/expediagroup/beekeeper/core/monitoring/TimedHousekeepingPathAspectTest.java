782cbac24fbd6922350ffcd03eaea93ec0af3647
max-jacobs <mjacobs@hotels.com>
2019-12-03 16:27:05 +0000

    Updating TimedHousekeepingPath and aspect and tests

--- beekeeper-core/src/test/java/com/expediagroup/beekeeper/core/monitoring/TimedHousekeepingPathAspectTest.java
+++ beekeeper-core/src/test/java/com/expediagroup/beekeeper/core/monitoring/TimedHousekeepingPathAspectTest.java
@@ -16,10 +16,12 @@
 package com.expediagroup.beekeeper.core.monitoring;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -31,16 +33,18 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.search.MeterNotFoundException;
 import io.micrometer.core.instrument.search.RequiredSearch;
 
 import com.expediagroup.beekeeper.core.TestApplication;
 import com.expediagroup.beekeeper.core.model.EntityHousekeepingPath;
+import com.expediagroup.beekeeper.core.model.HousekeepingPath;
 
 @ExtendWith(SpringExtension.class)
 @ExtendWith(MockitoExtension.class)
 @ContextConfiguration(classes = { TestApplication.class, MonitoredClass.class },
   loader = AnnotationConfigContextLoader.class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class TimedHousekeepingPathAspectTest {
 
   public static final String TIMER_NAME = "metric-timer";
@@ -48,23 +52,29 @@ public class TimedHousekeepingPathAspectTest {
   private static final String TABLE = "table";
   private static final String TABLE_2 = "table2";
 
+  private HousekeepingPath housekeepingPath;
+  
   @Autowired
   private MeterRegistry meterRegistry;
 
   @Autowired
   private MonitoredClass monitoredClass;
 
-  @Test
-  public void typicalTime() {
-    EntityHousekeepingPath path = new EntityHousekeepingPath.Builder()
+  @BeforeEach
+  public void init() {
+    housekeepingPath = new EntityHousekeepingPath.Builder()
       .databaseName(DATABASE)
       .tableName(TABLE)
       .creationTimestamp(LocalDateTime.now())
       .cleanupDelay(Duration.ofDays(1))
       .build();
-    monitoredClass.doSomething(path);
-    path.setTableName(TABLE_2);
-    monitoredClass.doSomething(path);
+  }
+  
+  @Test
+  public void typicalTime() {
+    monitoredClass.doSomething(housekeepingPath);
+    housekeepingPath.setTableName(TABLE_2);
+    monitoredClass.doSomething(housekeepingPath);
     Timer timer1 = RequiredSearch.in(meterRegistry)
       .name(TIMER_NAME)
       .tags("table", String.join(".", DATABASE, TABLE))
@@ -75,6 +85,25 @@ public class TimedHousekeepingPathAspectTest {
       .timer();
     assertThat(timer1).isNotNull();
     assertThat(timer2).isNotNull();
+  }
+
+  @Test
+  public void timeSucceedsWithMultipleArguments() {
+    monitoredClass.multipleArguments(housekeepingPath, 1, "test-string");
+    Timer timer = RequiredSearch.in(meterRegistry)
+      .name(TIMER_NAME)
+      .tags("table", String.join(".", DATABASE, TABLE))
+      .timer();
+    assertThat(timer).isNotNull();
+  }
+
+  @Test
+  public void timeFailsIfPathIsInWrongLocation() {
+    monitoredClass.pathIsNotTheFirstArg(1, "test-string", housekeepingPath);
+    assertThatThrownBy(() -> RequiredSearch.in(meterRegistry)
+      .name(TIMER_NAME)
+      .tags("table", String.join(".", DATABASE, TABLE))
+      .timer()).isInstanceOf(MeterNotFoundException.class);
   }
 
 }
