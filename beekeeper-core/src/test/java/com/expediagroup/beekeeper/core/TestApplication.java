982b6f8e12240b942db86e94cb857716ecaa5d28
max-jacobs <mjacobs@hotels.com>
2019-12-03 18:05:56 +0000

    Removed some unneeded config and excluding class from mycila plugin

--- beekeeper-core/src/test/java/com/expediagroup/beekeeper/core/TestApplication.java
+++ beekeeper-core/src/test/java/com/expediagroup/beekeeper/core/TestApplication.java
@@ -19,15 +19,13 @@ import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
-import org.springframework.boot.actuate.autoconfigure.metrics.web.client.HttpClientMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = { MetricsAutoConfiguration.class, HttpClientMetricsAutoConfiguration.class })
+@SpringBootApplication
 @EnableConfigurationProperties
 @ComponentScan("com.expediagroup.beekeeper.core")
 @EntityScan(basePackages = { "com.expediagroup.beekeeper.core.model" })
