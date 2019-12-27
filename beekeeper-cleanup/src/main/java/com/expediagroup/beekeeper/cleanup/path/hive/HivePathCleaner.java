1bd8677f940612c89348d559a3e0b38938c623fd
Ken Figueiredo <kfigueiredo@expediagroup.com>
2019-12-27 10:52:01 -0500

    Add timedtaggable to hive cleanup path

--- beekeeper-cleanup/src/main/java/com/expediagroup/beekeeper/cleanup/path/hive/HivePathCleaner.java
+++ beekeeper-cleanup/src/main/java/com/expediagroup/beekeeper/cleanup/path/hive/HivePathCleaner.java
@@ -1,7 +1,23 @@
+/**
+ * Copyright (C) 2019 Expedia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.expediagroup.beekeeper.cleanup.path.hive;
 
 import com.expediagroup.beekeeper.cleanup.path.PathCleaner;
 import com.expediagroup.beekeeper.core.model.HousekeepingPath;
+import com.expediagroup.beekeeper.core.monitoring.TimedTaggable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +32,7 @@ public class HivePathCleaner implements PathCleaner {
     }
 
     @Override
+    @TimedTaggable("hive-paths-deleted")
     public boolean cleanupPath(HousekeepingPath housekeepingPath) {
         if (LocalDateTime.now().isBefore(housekeepingPath.getCleanupTimestamp())) {
             log.debug("Not deleting path \"{}\", expiration timestamp not met yet.", housekeepingPath.getPath());
