d0afcb84a0affe3d933a7e18357e444a12750052
JayGreeeen <jgreenstevens@expediagroup.com>
2020-07-02 13:07:07 +0100

    Initial commit

--- beekeeper-metadata-cleanup/src/main/java/com/expediagroup/beekeeper/metadata/cleanup/table/MetadataCleaner.java
+++
@@ -1,9 +0,0 @@
-package com.expediagroup.beekeeper.metadata.cleanup.table;
-
-import com.expediagroup.beekeeper.core.model.HousekeepingPath;
-
-public interface MetadataCleaner {
-
-  void cleanupMetadata(HousekeepingPath housekeepingPath);
-
-}
