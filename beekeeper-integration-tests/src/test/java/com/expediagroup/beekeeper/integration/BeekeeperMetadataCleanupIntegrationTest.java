d6436d64d90c11f34687890ef9f35b672eb5f057
JayGreeeen <jgreenstevens@expediagroup.com>
2020-07-24 17:43:53 +0100

    Initial commit. Created class

---
+++ beekeeper-integration-tests/src/test/java/com/expediagroup/beekeeper/integration/BeekeeperMetadataCleanupIntegrationTest.java
@@ -0,0 +1,28 @@
+package com.expediagroup.beekeeper.integration;
+
+public class BeekeeperMetadataCleanupIntegrationTest {
+
+  // TODO
+  // add some metadata to the HousekeepingMetadata table
+  // test the cleanup for partitioned tables
+  // test the cleanup for unpartitioned tables
+  // check that paths are still deleted if the table doesnt exist
+  // check that paths are still deleted if the partition doesnt exist
+
+  // setup
+  // need some kind of hive object can test with
+
+  // need to have some hive test utils
+  // will have a hive metastore
+  // like circus train
+  // // create unpartitioned table
+  // // create partitioned table
+
+  // will also need to make use of the amazon s3 stuff
+  // set up some paths
+  // make sure theyre deleted
+
+  // use the integration test base
+  // insertExpiredMetadata
+
+}
