a8e9dfd2e28907c41507da1dec93e4f5c73d7431
shermosa <shermosa@hotels.com>
2021-03-24 11:15:15 +0000

    deleting useless stuff

--- beekeeper-api/src/main/java/com/expediagroup/beekeeper/api/BeekeeperController.java
+++ beekeeper-api/src/main/java/com/expediagroup/beekeeper/api/BeekeeperController.java
@@ -23,13 +23,5 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BeekeeperController {
-
-  private static final String template = "Hello, %s!";
-  private final AtomicLong counter = new AtomicLong();
-
-  @RequestMapping("/testing")
-  public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-    return new Greeting(counter.incrementAndGet(), String.format(template, name));
-  }
   
 }
