2f64dffd6024d29fbd92d65e88593825bf256616
shermosa <shermosa@hotels.com>
2021-03-24 11:04:13 +0000

    deleted useless class

--- beekeeper-api/src/main/java/com/expediagroup/beekeeper/api/BeekeeperController.java
+++ beekeeper-api/src/main/java/com/expediagroup/beekeeper/api/BeekeeperController.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2019-2021 Expedia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.expediagroup.beekeeper.api;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -12,8 +27,9 @@ public class BeekeeperController {
   private static final String template = "Hello, %s!";
   private final AtomicLong counter = new AtomicLong();
 
-  @RequestMapping("/testing/v1")
+  @RequestMapping("/testing")
   public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
     return new Greeting(counter.incrementAndGet(), String.format(template, name));
   }
+  
 }
