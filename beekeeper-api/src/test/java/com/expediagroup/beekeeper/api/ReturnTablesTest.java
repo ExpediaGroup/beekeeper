2a3a6338c056cd8544609047a2d12459a738d827
shermosa <shermosa@hotels.com>
2021-03-25 16:01:20 +0000

    remove unused imports

--- beekeeper-api/src/test/java/com/expediagroup/beekeeper/api/ReturnTablesTest.java
+++ beekeeper-api/src/test/java/com/expediagroup/beekeeper/api/ReturnTablesTest.java
@@ -1,38 +1,13 @@
 package com.expediagroup.beekeeper.api;
 
-import static com.expediagroup.beekeeper.core.model.LifecycleEventType.UNREFERENCED;
-
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.query.Param;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-import com.expediagroup.beekeeper.cleanup.aws.S3PathCleaner;
-import com.expediagroup.beekeeper.cleanup.hive.HiveMetadataCleaner;
-import com.expediagroup.beekeeper.cleanup.metadata.CleanerClient;
-import com.expediagroup.beekeeper.cleanup.path.PathCleaner;
 import com.expediagroup.beekeeper.core.model.HousekeepingMetadata;
-import com.expediagroup.beekeeper.core.model.HousekeepingPath;
-import com.expediagroup.beekeeper.core.model.LifecycleEventType;
 import com.expediagroup.beekeeper.core.repository.HousekeepingMetadataRepository;
-import com.expediagroup.beekeeper.core.repository.HousekeepingPathRepository;
-import com.expediagroup.beekeeper.path.cleanup.handler.UnreferencedPathHandler;
 
 @ExtendWith(MockitoExtension.class)
 public class ReturnTablesTest {
