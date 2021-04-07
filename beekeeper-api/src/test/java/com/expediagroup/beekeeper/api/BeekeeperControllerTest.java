36e4cac490d35a1b5be8c7f39de5b0240ec52620
shermosa <shermosa@hotels.com>
2021-04-07 14:23:52 +0100

    pom

--- beekeeper-api/src/test/java/com/expediagroup/beekeeper/api/BeekeeperControllerTest.java
+++ beekeeper-api/src/test/java/com/expediagroup/beekeeper/api/BeekeeperControllerTest.java
@@ -100,7 +100,7 @@ public class BeekeeperControllerTest {
         .lifecycleType(EXPIRED.toString())
         .build();
   }
-  
+
   @Test
   public void getAllTables() throws Exception {
     List<HousekeepingMetadata> tablesList = new ArrayList<HousekeepingMetadata>();
