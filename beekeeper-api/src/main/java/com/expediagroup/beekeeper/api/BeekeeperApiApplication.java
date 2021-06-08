721b7d5fcf347d5a93a3e03f83675357c11771cd
shermosa <shermosa@hotels.com>
2021-06-08 11:04:26 +0100

    useless line

--- beekeeper-api/src/main/java/com/expediagroup/beekeeper/api/BeekeeperApiApplication.java
+++ beekeeper-api/src/main/java/com/expediagroup/beekeeper/api/BeekeeperApiApplication.java
@@ -15,9 +15,7 @@
  */
 package com.expediagroup.beekeeper.api;
 
-import javax.annotation.PostConstruct;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -25,8 +23,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @SpringBootApplication
 @EnableConfigurationProperties
@@ -37,17 +33,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
     "com.expediagroup.beekeeper.api.controller",
     "com.expediagroup.beekeeper.api.service" })
 public class BeekeeperApiApplication {
-
-  @Autowired
-  private ObjectMapper objectMapper;
-  
   public static void main(String[] args) {
     SpringApplication.run(BeekeeperApiApplication.class, args);
   }
-  
-  @PostConstruct
-  public void setUp() {
-    objectMapper.registerModule(new JavaTimeModule());
-  }
-
 }
