b5ae372385ffe518fcab74ca78e3f2b3e87200a2
shermosa <shermosa@hotels.com>
2021-07-06 13:56:12 +0100

    add spec test

--- beekeeper-api/src/test/java/com/expediagroup/beekeeper/api/controller/BeekeeperControllerTest.java
+++ beekeeper-api/src/test/java/com/expediagroup/beekeeper/api/controller/BeekeeperControllerTest.java
@@ -16,6 +16,8 @@
 package com.expediagroup.beekeeper.api.controller;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
@@ -32,12 +34,16 @@ import static com.expediagroup.beekeeper.api.util.DummyHousekeepingMetadataGener
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -61,6 +67,9 @@ public class BeekeeperControllerTest {
 
   @MockBean
   private HousekeepingEntityServiceImpl housekeepingEntityServiceImpl;
+
+  @Captor
+  ArgumentCaptor<Specification<HousekeepingMetadata>> specCaptor;
 
   @Test
   public void testGetAllMetadataWhenValidInput() throws Exception {
@@ -86,6 +95,29 @@ public class BeekeeperControllerTest {
         .perform(get("/api/v1/database/some_database/table/some_table/metadataa"))
         .andDo(MockMvcResultHandlers.print())
         .andExpect(status().isNotFound());
+  }
+
+  @Test
+  public void testGetAllMetadataWhenThereIsSpecification() throws Exception {
+    HousekeepingMetadata metadata = generateDummyHousekeepingMetadata("some_database", "some_table");
+    HousekeepingMetadataResponse metadataResponse = convertToHousekeepingMetadataResponse(metadata);
+    Page<HousekeepingMetadataResponse> metadataResponsePage = new PageImpl<>(List.of(metadataResponse, metadataResponse));
+
+    when(housekeepingEntityServiceImpl.getAllMetadata(any(), any())).thenReturn(metadataResponsePage);
+
+    mockMvc
+        .perform(get("/api/v1/database/some_database/table/some_table/metadata?partition_name=event_date=2020-01-01/event_hour=0/event_type=B&lifecycle_type=UNREFERENCED"))
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(content().json(objectMapper.writeValueAsString(metadataResponsePage)));
+    verify(housekeepingEntityServiceImpl, times(1)).getAllMetadata(any(), any());
+    verifyNoMoreInteractions(housekeepingEntityServiceImpl);
+
+    Mockito.verify(housekeepingEntityServiceImpl).getAllMetadata(specCaptor.capture(), any());
+    Specification<HousekeepingMetadata> result = specCaptor.getValue();
+    String spec = "Conjunction [innerSpecs=[EmptyResultOnTypeMismatch [wrappedSpec=EqualIgnoreCase [expectedValue=some_table, converter=Converter [dateFormat=null, onTypeMismatch=EMPTY_RESULT], path=tableName]], EmptyResultOnTypeMismatch [wrappedSpec=EqualIgnoreCase [expectedValue=some_database, converter=Converter [dateFormat=null, onTypeMismatch=EMPTY_RESULT], path=databaseName]], EmptyResultOnTypeMismatch [wrappedSpec=EqualIgnoreCase [expectedValue=event_date=2020-01-01/event_hour=0/event_type=B, converter=Converter [dateFormat=null, onTypeMismatch=EMPTY_RESULT], path=partitionName]], EmptyResultOnTypeMismatch [wrappedSpec=EqualIgnoreCase [expectedValue=UNREFERENCED, converter=Converter [dateFormat=null, onTypeMismatch=EMPTY_RESULT], path=lifecycleType]]]]";
+    assertThat(result.toString()).isEqualTo(spec);
   }
 
   @Test
