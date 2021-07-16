21ce01c66ba2422af956885f9a23be74ebb6562d
shermosa <shermosa@hotels.com>
2021-07-16 12:20:09 +0100

    add pageable assertion

--- beekeeper-api/src/test/java/com/expediagroup/beekeeper/api/response/HousekeepingMetadataResponseTest.java
+++ beekeeper-api/src/test/java/com/expediagroup/beekeeper/api/response/HousekeepingMetadataResponseTest.java
@@ -16,15 +16,10 @@
 package com.expediagroup.beekeeper.api.response;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import static com.expediagroup.beekeeper.api.response.MetadataResponseConverter.convertToHousekeepingMetadataResponsePage;
 import static com.expediagroup.beekeeper.api.util.DummyHousekeepingMetadataGenerator.generateDummyHousekeepingMetadata;
-import static com.expediagroup.beekeeper.core.model.HousekeepingStatus.SCHEDULED;
-import static com.expediagroup.beekeeper.core.model.LifecycleEventType.EXPIRED;
-
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -32,19 +27,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 
 import com.expediagroup.beekeeper.core.model.HousekeepingMetadata;
-import com.expediagroup.beekeeper.core.model.HousekeepingStatus;
 
 public class HousekeepingMetadataResponseTest {
-
-  private static final String databaseName = "randomDatabase";
-  private static final String tableName = "randomTable";
-  private static final String path = "s3://some/path/";
-  private static final String partitionName = null;
-  private static final HousekeepingStatus housekeepingStatus = SCHEDULED;
-  private static final LocalDateTime creationTimestamp = LocalDateTime.now(ZoneId.of("UTC"));
-  private static final Duration cleanupDelay = Duration.parse("P3D");
-  private static final int cleanupAttempts = 0;
-  private static final String lifecycleEventType = EXPIRED.toString();
 
   @Test
   public void testConvertToHouseKeepingMetadataResponsePage() {
@@ -58,6 +42,7 @@ public class HousekeepingMetadataResponseTest {
     List<HousekeepingMetadataResponse> metadataResponsePageList = metadataResponsePage.getContent();
     assertThat(metadataResponsePageList.get(0).getDatabaseName()).isEqualTo(metadata1.getDatabaseName());
     assertThat(metadataResponsePageList.get(1).getDatabaseName()).isEqualTo(metadata2.getDatabaseName());
+    assertTrue(metadataResponsePage.getPageable().equals((new PageImpl<>(housekeepingMetadataList).getPageable())));
   }
 
 }
