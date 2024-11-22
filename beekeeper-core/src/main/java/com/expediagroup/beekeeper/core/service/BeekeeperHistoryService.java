a938ee47b69c924c64a000369f1751ac9684205a
jgreenstevens <jgreenstevens@expediagroup.com>
2024-11-22 16:27:52 +0000

    Annotations

--- beekeeper-core/src/main/java/com/expediagroup/beekeeper/core/service/BeekeeperHistoryService.java
+++ beekeeper-core/src/main/java/com/expediagroup/beekeeper/core/service/BeekeeperHistoryService.java
@@ -4,17 +4,21 @@ import java.time.LocalDateTime;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.expediagroup.beekeeper.core.model.HousekeepingEntity;
 import com.expediagroup.beekeeper.core.model.history.BeekeeperHistory;
 import com.expediagroup.beekeeper.core.repository.BeekeeperHistoryRepository;
 
+@Component
 public class BeekeeperHistoryService {
 
   private static final Logger log = LoggerFactory.getLogger(BeekeeperHistoryService.class);
 
   private final BeekeeperHistoryRepository beekeeperHistoryRepository;
 
+  @Autowired
   public BeekeeperHistoryService(BeekeeperHistoryRepository beekeeperHistoryRepository) {
     this.beekeeperHistoryRepository = beekeeperHistoryRepository;
   }
