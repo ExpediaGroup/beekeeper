d995643824d2f13ace90b3b04e6b580f680b69cd
Hamza Jugon <hjugon@expediagroup.com>
2025-06-16 23:13:27 +0100

    Add JavaDoc for new method

--- beekeeper-scheduler/src/main/java/com/expediagroup/beekeeper/scheduler/hive/HiveClient.java
+++ beekeeper-scheduler/src/main/java/com/expediagroup/beekeeper/scheduler/hive/HiveClient.java
@@ -76,6 +76,15 @@ public class HiveClient implements Closeable {
     }
   }
 
+  /**
+   * Retrieves information for a single partition from Hive metastore.
+   *
+   * @param databaseName the name of the Hive database
+   * @param tableName the name of the Hive table
+   * @param partitionName the partition identifier in Hive's standard format: "key1=value1/key2=value2"
+   *                      (e.g., "event_date=2024-01-01/event_hour=1")
+   * @return Optional containing PartitionInfo if found, empty if partition doesn't exist or on error
+   */
   public Optional<PartitionInfo> getSinglePartitionInfo(String databaseName, String tableName, String partitionName) {
     try {
       List<String> partitionValues = Warehouse.getPartValuesFromPartName(partitionName);
